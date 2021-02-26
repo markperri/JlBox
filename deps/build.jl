91e81409b89d3a8948d93f5a3d40a15a8ccd3344
Mark Perri <mark.perri@sonoma.edu>
2021-02-26 15:57:46 +0000

    delete/cleared

--- deps/build.jl
+++ deps/build.jl
@@ -14,17 +14,5 @@ if unsatisfied
 end
 #BinaryProvider.write_deps_file(joinpath(@__DIR__, "deps.jl"), products) #do not need libdl
 
-ENV["PYTHON"]=""
 Pkg.build("PyCall")
 
-using Conda
-#Conda.exists failed anyway
-#if Conda.exists("openbabel==2.4.1")#Conda.exists("openbabel") doesn't work now!
-    Conda.add("openbabel",channel="conda-forge")
-#end
-root = Conda.ROOTENV
-condaPath = Sys.iswindows() ? joinpath(root, "Scripts", "conda.exe") : joinpath(root, "bin", "conda")
-run(`$(condaPath) install -y Werkzeug=0.16.0`)
-Conda.add("flask")
-Conda.add("flask-wtf")
-Conda.add("xlsxwriter")
